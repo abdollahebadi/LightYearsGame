8018c90ba624bb63fb4399f7a1e21cd12cf991db
Abdollah Ebadi <a.ebadi@boundless-systems.com.au>
2026-04-15 14:42:43 +1000

    Create top level CMakeLists.txt and src folder and its cmakelists.txt file.

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+// In the name of Allah
+// Created by abdollah-ebadi on 4/14/26.
+//
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Hello Light years..." << std::endl ;
+}
