ace6fc5648dfdd143adcf038663882ba43aff043
Abdollah Ebadi <a.ebadi@boundless-systems.com.au>
2026-04-16 16:41:57 +1000

    add new source files and change structure accordingly and change cmake config to not always download the SFML source files.

--- build.sh
+++ build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 BUILD_DIR="./build"
+DEPS_DIR="./_deps"
 DEFAULT_INSTALL_DIR="./install"
 
-if [ "$1" == "clean" ]; then
+if [ "$1" == "clean-build" ]; then
     echo "Cleaning build and install directories..."
     rm -rf "$BUILD_DIR"
     rm -rf "$DEFAULT_INSTALL_DIR"
     echo "Clean complete."
+elif [ "$1" == "clean-all" ]; then
+    echo "Cleaning build, install and dependencies directories..."
+    rm -rf "$BUILD_DIR"
+    rm -rf "$DEFAULT_INSTALL_DIR"
+    rm -rf "$DEPS_DIR"
+    echo "Clean all complete."
 fi
 
 mkdir -p "$BUILD_DIR"
@@ -19,7 +26,6 @@ echo "Where would you like to install the game?"
 echo "Press Enter to use the default: $DEFAULT_INSTALL_DIR"
 read -p "Install path: " USER_INSTALL_DIR
 
-# Use default if user just pressed Enter
 if [ -z "$USER_INSTALL_DIR" ]; then
     INSTALL_DIR="../$DEFAULT_INSTALL_DIR"
 else
--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,24 @@
 #include <SFML/Graphics.hpp>
 
 int main(int argc, char** argv) {
-    // std::cout << "Hello Light years..." << std::endl ;
 
+    // Create the window to show on the screen.
     sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
+
+    // Create the circle and set its color.
     sf::CircleShape shape( 100.f );
     shape.setFillColor( sf::Color::Green );
 
+    // This is the main loop of the window, that will run if the state of the window is open.
     while ( window.isOpen() )
     {
+        // We are polling for any event that happens in the context of this window.
         while ( const std::optional event = window.pollEvent() )
         {
+            // The window will not close automatically with just getting the close event
+            // We need to close the window manually.
             if ( event->is<sf::Event::Closed>() )
-                window.close();
+                window.close(); // This should change the state of the window to close.
         }
 
         window.clear();
@@ -25,4 +31,7 @@ int main(int argc, char** argv) {
         window.display();
     }
 
+    if (!window.isOpen()) {
+        std::cout << "Window is closed, we are shutting down...!" << std::endl ;
+    }
 }
